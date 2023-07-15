4fc5d5976694041137e5c587b3566629ebda1a79
Alexis Urtecho Ocampo <alexis.urtecho@meru.com>
2023-07-15 17:31:55 -0600

    remove home spec

--- spec/requests/intentions_spec.rb
+++ spec/requests/intentions_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "Intentions", type: :request do
   describe "POST /create" do
+    let(:input_text) { 'Prueba' }
     it "returns http success" do
       post "/intentions"
       expect(response).to have_http_status(:success)
