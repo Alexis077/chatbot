323e2b4b736f67884a9791aa1fa03d4fd83a0c2c
Alexis Urtecho Ocampo <alexis.urtecho@meru.com>
2023-07-15 17:30:45 -0600

    add fix spec

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -2,8 +2,9 @@ require 'rails_helper'
 
 RSpec.describe "Homes", type: :request do
   describe "GET /index" do
+    let(:input_text) { 'Prueba' }
     it "returns http success" do
-      get "/"
+      post "/intentions"
       expect(response).to have_http_status(:success)
     end
   end
