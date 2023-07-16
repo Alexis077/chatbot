a35e5d8e827691cce87c42cd040f0aaa0a674061
Alexis Urtecho Ocampo <alexis.urtecho@meru.com>
2023-07-15 19:41:40 -0600

    validate message cannot be blank

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,5 @@
 class Message < ApplicationRecord
+  validates :text, allow_blank: false, presence: true
+
   belongs_to :chat_session
 end
--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -2,6 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Message, type: :model do
   describe "associations" do
+
+    it "raises an exception with a blank message" do
+      chat_session = FactoryBot.create(:chat_session)
+      expect {
+        FactoryBot.create(:message, chat_session: chat_session, text: "")
+      }.to raise_error(ActiveRecord::RecordInvalid)
+    end
+    
     it "belongs to a chat session" do
       chat_session = FactoryBot.create(:chat_session)
       message = FactoryBot.create(:message, chat_session: chat_session)
